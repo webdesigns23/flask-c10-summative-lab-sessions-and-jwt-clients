dc0e43b90f645ccc63271d3100fb59db0d65f923
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-01 12:18:03 -0600

    added validations/constraints and  password hash functionality

--- server/models.py
+++ server/models.py
@@ -1,29 +1,61 @@
-from config import db
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt
 
+# Model for wine cellar app users
 class User(db.Model):
 	__tablename__ = "users"
 
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String)
+	username = db.Column(db.String, unique=True, nullable=False)
 	_password_hash = db.Column(db.String)
 	image_url = db.Column(db.String)
 
 	cellar_records = db.relationship('CellarRecord', back_populates='user')
 
+	@hybrid_property
+	def password_hash(self):
+		raise AttributeError('Password hashes may not be viewed')
+    
+	@password_hash.setter
+	def password_hash(self, password):
+		password_hash = bcrypt.generate_password_hash(
+			password.encode('utf-8'))
+		self._password_hash = password_hash.decode('utf-8')
+         
+	def authenticate(self, password):
+		return bcrypt.check_password_hash(
+			self._password_hash, password.encode('utf-8'))
+
 	def __repr__(self):
-		return f'<User {self.username}, {self.image_url}>'
+		return f'<User: {self.username}, {self.image_url}>'
 
-
+# Model stores wine entries for users
 class CellarRecord(db.Model):
 	__tablename__ = "cellar_records"
 
 	id = db.Column(db.Integer, primary_key=True)
-	wine = db.Column(db.String)
-	producer = db.Column(db.String)
-	country = db.Column(db.String)
-	vintage = db.Column(db.Integer)
-	quantity = db.Column(db.Integer)
-	tasting_notes = db.Column(db.String)
+	wine = db.Column(db.String, nullable=False)
+	grape = db.Column(db.String, nullable=False)
+	country = db.Column(db.String, nullable=False)
+	vintage = db.Column(db.Integer, nullable=False)
+	quantity = db.Column(db.Integer, default=1)
+	tasting_notes = db.Column(db.String, nullable=True)
 
 	user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
 	user = db.relationship('User', back_populates='cellar_records')
+
+	@validates('quantity')
+	def validate_quantity(self, key, quantity):
+		if quantity < 0:
+			raise ValueError("Quantity cannot be a negative amount.")
+		return quantity
+
+	@validates('tasting_notes')
+	def validate_tasting_notes(self, key, tasting_notes):
+		if tasting_notes and len(tasting_notes) > 65:
+			raise ValueError("Tasting notes cannot be longer than 65 characters.")
+		return tasting_notes
+
+	def __repr__(self):
+		return f'<Cellar Record: {self.wine}, {self.producer}, {self.country}, {self.vintage}, {self.quantity}, {self.tasting_notes}>'
