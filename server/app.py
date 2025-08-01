95a33f645b43eda10ab599cf2180f254e69ecebe
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-01 14:26:24 -0600

    seeded data and confirmed passwords are hashed before being saved to db

---
+++ server/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from config import app, db, api
+from models import User, CellarRecord, UserSchema, CellarRecordSchema
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
