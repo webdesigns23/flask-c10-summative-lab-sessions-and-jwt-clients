5777329365a6f1ddf02f7c7e6eba350b0668e286
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-01 15:16:12 -0600

    added Signup and Check Session classes with endpoints

--- server/app.py
+++ server/app.py
@@ -17,6 +17,29 @@ class Signup(Resource):
         data = request.get_json()
         
         username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url')
+        
+        user = User(username = username, image_url = image_url)
+        user.password_hash = password
+        
+        try:
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 201
+        except IntegrityError:
+            return {'error': 'Unprocessable Entity'}, 422
+        
+class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return UserSchema().dump(user), 200
+        return {'error': 'User is not logged in'}, 401
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
